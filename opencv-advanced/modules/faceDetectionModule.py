71893f09e5d5c1b77a80b5cbbe77e70652f9ce0c
Harish Sivaraman <dir.harishsivaraman@gmail.com>
2025-06-26 10:14:48 +0530

    face detection fancy draw added

--- opencv-advanced/modules/faceDetectionModule.py
+++ opencv-advanced/modules/faceDetectionModule.py
@@ -13,39 +13,60 @@ class FaceDetection():
 
     def findFace(self,img,draw=True):
         imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        results = self.faceDectections.process(imgRGB)
-        if results.detections:
-            if draw:
-                for id,detection in enumerate(results.detections):
+        self.results = self.faceDectections.process(imgRGB)
+        bboxs = []
+        if self.results.detections:
+            for id,detection in enumerate(self.results.detections):
                     bboxC = detection.location_data.relative_bounding_box
                     ih,iw,ic = img.shape
                     bbox = int(bboxC.xmin * iw) , int(bboxC.ymin * ih), \
                            int(bboxC.width * iw) , int(bboxC.height * ih)
-                    cv2.rectangle(img,bbox,(255,0,255),2)
+                    bboxs.append([id,bbox,detection.score])
+            if draw:
+                    img = self.fancyDraw(img,bbox)
                     cv2.putText(img,f"{int(detection.score[0]*100)}%",(bbox[0],bbox[1]-20),cv2.FONT_HERSHEY_PLAIN,3,(0,255,255),2)
-        return img     
-
+        return img,bboxs
+    
+    def fancyDraw(self,img,bbox,l=30,t=5,rt=1):
+         x,y,w,h = bbox
+         x1 , y1 = x+w,y+h
+         cv2.rectangle(img,bbox,(255,0,255),rt)
+         # top left x,y
+         cv2.line(img,(x,y),(x+l,y),(255,0,255),t)
+         cv2.line(img,(x,y),(x,y+l),(255,0,255),t)
+         #top right x1,y
+         cv2.line(img,(x1,y),(x1-l,y),(255,0,255),t)
+         cv2.line(img,(x1,y),(x1,y+l),(255,0,255),t)
+         #bottom left
+         cv2.line(img,(x,y1),(x+l,y1),(255,0,255),t)
+         cv2.line(img,(x,y1),(x,y1-l),(255,0,255),t)
+         #bottom right
+         cv2.line(img,(x1,y1),(x1-l,y1),(255,0,255),t)
+         cv2.line(img,(x1,y1),(x1,y1-l),(255,0,255),t)
+         return img
 
 def main():
     pTime = 0
     cTime = 0
     
-    cap = cv2.VideoCapture(r'../clips/face.mp4')
+    cap = cv2.VideoCapture(0)
     monitor = get_monitors()[0]
     detector = FaceDetection()
     scrWidth, scrHeight = monitor.width, monitor.height
 
     while True:
         success, img = cap.read()
-        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        img  = detector.findFace(img)
+        img,bboxs  = detector.findFace(img)
+        print(bboxs)
+        cTime = time.time() 
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
         h,w,_ = img.shape
         scale = min(scrWidth / w*0.5 , scrHeight /h*0.5) 
         resized_img = cv2.resize(img,(int(w*scale), int(h*scale)))
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(resized_img,f"FPS: {int(fps)}",(70,50),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),2)
+
+        cv2.putText(resized_img,f"FPS: {int(fps)}",(30,50),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),2)
         cv2.imshow("img",resized_img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break       
